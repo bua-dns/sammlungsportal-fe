8b8f05887ba99f8e10393b32afd316a06596ee7d
grandgeorg <viktor@grandgeorg.de>
2023-05-10 09:16:59 +0200

    add local hero image

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,8 @@
   </Head>
   <!-- <h1 class="text-center">{{ theme.collection_portal }}</h1> -->
   <div class="hero">
-    <img style="width:100%;height:auto;" src="https://unsplash.it/1920/1080" alt="hero image" />
+    <img style="width:100%;height:auto;" src="@/assets/img/Collage_01.png" alt="hero image" />
+    <!-- <img style="width:100%;height:auto;" src="https://unsplash.it/1920/1080" alt="hero image" /> -->
   </div>
   <div class="collection_cards_wrapper">
     <CollectionCard v-for="collection in data" :key="collection.id" :collection="collection" />
