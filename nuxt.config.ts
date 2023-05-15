5688649d7e2a7867bdaf74c132a478f04920aa1a
grandgeorg <viktor@grandgeorg.de>
2023-05-15 09:32:20 +0200

    add app icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,22 @@ export default defineNuxtConfig({
   app: {
     head: {
       htmlAttrs: { lang: "de" },
+      meta: [
+        // { charset: "utf-8" },
+        // { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "msapplication-TileColor", content: "#2d89ef" },
+        { name: "msapplication-config", content: "/browserconfig.xml" },
+        { name: "theme-color", content: "#ffffff" },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/img/icons/favicon.ico' },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/img/icons/apple-touch-icon.png" },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/img/icons/favicon-32x32.png" },
+        { rel: "icon", type: "image/png", sizes: "16x16", href: "/img/icons/favicon-16x16.png" },
+        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "mask-icon", href: "/img/icons/safari-pinned-tab.svg", color: "#5bbad5" },
+        { rel: "shortcut icon", href: "/img/icons/favicon.ico" },
+      ],
     },
   },
   css: [
