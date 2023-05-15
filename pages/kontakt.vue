cbaff172450607612822ea6d2a2627bf0efe9805
grandgeorg <viktor@grandgeorg.de>
2023-05-15 09:34:30 +0200

    add markdown from API

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -1,7 +1,9 @@
 <template>
   <h1 class="text-center">{{ w.page_contact }}</h1>
+  {{ data.content }}
 </template>
 <script setup>
   const theme = useState('theme');
   const w = theme.value.data.wording.de;
+  const { data } = await useFetch('https://sammlungsportal.bua-dns.de/items/pages?filter[slug][_eq]=contact');
 </script>
