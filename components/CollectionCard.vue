e25478546af80f05fcee475d2b5389c4aa30f852
grandgeorg <viktor@grandgeorg.de>
2023-10-11 19:53:45 +0200

    add grid view

--- components/CollectionCard.vue
+++ components/CollectionCard.vue
@@ -2,6 +2,22 @@
   defineProps(['entry']);
   const theme = useState('theme');
   const w = theme.value.data.wording.de;
+  function getUniMarkerStyles(color) {
+    return typeof color !== "undefined"
+      ? "background-color: " + color + ";"
+      : "";
+  }
+  function getUniMarkerColors(current_keeper) {
+    const status = theme.value.data.names.keepers.find((keeper) => keeper.name == current_keeper);
+    theme.value.data.settings.ciColors[status.id];
+    return getUniMarkerStyles(theme.value.data.settings.ciColors[status.id]);
+  }
+
+  function stripEntry(entry) {
+    // strip '["' and '"]' from entry
+    return entry.replace(/(\["|"\])/g, "");
+  }
+
 </script>
 <template>
   <dl class="card">
@@ -11,7 +27,9 @@
     </template>
     <template v-if="entry.current_keeper">
       <dt>{{ w.current_keeper }}</dt>
-      <dd>{{ entry.current_keeper }}</dd>
+      <dd>
+        <span class="gws_uni_marker" :style="getUniMarkerColors(entry.current_keeper)"></span>
+        {{ entry.current_keeper }}</dd>
     </template>
     <template v-if="entry.description">
       <dt>{{ w.description }}</dt>
@@ -19,11 +37,13 @@
     </template>
     <template v-if="entry.phone">
       <dt>{{ w.phone }}</dt>
-      <dd>{{ entry.phone }}</dd>
+      <dd>
+        <a :href="'tel:' + stripEntry(entry.phone)">{{ stripEntry(entry.phone) }}</a>
+      </dd>
     </template>
     <template v-if="entry.email">
       <dt>{{ w.email }}</dt>
-      <dd>{{ entry.email }}</dd>
+      <dd><a :href="'mailto:' + stripEntry(entry.email)">{{ stripEntry(entry.email) }}</a></dd>
     </template>
     <template v-if="entry.homepage">
       <dt>{{ w.homepage }}</dt>
@@ -46,6 +66,34 @@
         <a :href="portal.url" target="_blank" rel="noopener">{{ portal.name }}</a>
       </dd>
     </template>
+    <template v-if="entry.role && entry.role.length > 0">
+      <dt>{{ w.role }}</dt>
+      <dd v-for="role in entry.role" :key="'role_' + entry.id + '_' + role.id">{{ role.label }}</dd>
+    </template>
+    <template v-if="entry.subject && entry.subject.length > 0">
+      <dt>{{ w.subject }}</dt>
+      <dd v-for="subject in entry.subject" :key="'subject_' + entry.id + '_' + subject.id">{{ subject.label }}</dd>
+    </template>
+    <template v-if="entry.genre && entry.genre.length > 0">
+      <dt>{{ w.genre }}</dt>
+      <dd v-for="genre in entry.genre" :key="'genre_' + entry.id + '_' + genre.id">{{ genre.label }}</dd>
+    </template>
+    <template v-if="entry.used_in_activity && entry.used_in_activity.length > 0">
+      <dt>{{ w.used_in_activity }}</dt>
+      <dd v-for="used_in_activity in entry.used_in_activity" :key="'used_in_activity_' + entry.id + '_' + used_in_activity.id">{{ used_in_activity.label }}</dd>
+    </template>
+    <template v-if="entry.academic_teaching && entry.academic_teaching.length > 0">
+      <dt>{{ w.academic_teaching }}</dt>
+      <dd v-for="(academic_teaching, idx) in entry.academic_teaching" :key="'academic_teaching_' + entry.id + '_' + idx">{{ academic_teaching.label }}</dd>
+    </template>
+    <template v-if="entry.special_form && entry.special_form.length > 0">
+      <dt>{{ w.special_form }}</dt>
+      <dd v-for="special_form in entry.special_form" :key="'special_form_' + entry.id + '_' + special_form.id">{{ special_form.label }}</dd>
+    </template>
+    <template v-if="entry.living_being && entry.living_being.length > 0">
+      <dt>{{ w.living_being }}</dt>
+      <dd v-for="living_being in entry.living_being" :key="'living_being_' + entry.id + '_' + living_being.id">{{ living_being.label }}</dd>
+    </template>
   </dl>
 </template>
 <style scoped lang="scss">
@@ -53,6 +101,8 @@
     padding: 1rem;
     border: 1px solid #ccc;
     border-radius: 8px;
+    background-color: #fff;
+    opacity: 0.85;
   }
   dt, dd {
   overflow-wrap: break-word;
@@ -63,5 +113,13 @@
   }
   a {
     word-wrap: break-word;
+  }
+  .gws_uni_marker {
+    width: 20px;
+    height: 20px;
+    display: inline-block;
+    vertical-align: text-bottom;
+    margin-right: .25rem;
+    border-radius: 100%;
   }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,174 @@
 <script setup>
-  const { data } = await useFetch('https://sammlungsportal.bua-dns.de/items/bua_collections?limit=-1&meta=total_count');
+  const { data } = await useFetch('https://sammlungsportal.bua-dns.de/items/bua_collections?limit=-1&sort=label&meta=total_count');
   const theme = useState('theme');
   const w = theme.value.data.wording.de;
+  const cardType = ref("grid");
+  function toggleCardType() {
+    cardType.value = cardType.value == "list" ? "grid" : "list";
+  }
 </script>
 <template>
   <Head>
     <Title>{{ w.page_start }}</Title>
   </Head>
-  <!-- <h1 class="text-center">{{ theme.collection_portal }}</h1> -->
+  <!--
   <div class="hero">
     <img style="width:100%;height:auto;" src="@/assets/img/Collage_01.png" alt="hero image" />
   </div>
-  <div class="collection_count">{{ w.num_collections }}: {{ data.meta.total_count }}</div>
+  -->
+  <div class="grid-control-bar">
+    <div class="collections-counter">{{ w.num_collections }}: {{ data.meta.total_count }}</div>
+    <div class="sort-controls">
+        <div class="input-group">
+          <label for="tag-cloud-sort-select" class="sort-label">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-list"
+              viewBox="0 0 16 16">
+              <path
+                d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z">
+              </path>
+              <path
+                d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z">
+              </path>
+            </svg>
+            <span class="description">sort by</span>
+          </label>
+          <select id="tag-cloud-sort-select" class="sort-select">
+            <option value="name">name</option>
+            <option value="count">count</option>
+          </select>
+        </div>
+        <div class="input-group">
+          <label for="tag-cloud-sort-order" class="order-label">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-alpha-down"
+              viewBox="0 0 16 16">
+              <path fill-rule="evenodd"
+                d="M10.082 5.629 9.664 7H8.598l1.789-5.332h1.234L13.402 7h-1.12l-.419-1.371h-1.781zm1.57-.785L11 2.687h-.047l-.652 2.157h1.351z">
+              </path>
+              <path
+                d="M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645V14zM4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293V2.5z">
+              </path>
+            </svg>
+            <span class="description">
+              order
+            </span>
+          </label>
+          <select id="tag-cloud-sort-order" class="order-select">
+            <option value="asc">ascending</option>
+            <option value="desc">descending</option>
+          </select>
+        </div>
+    </div>
+    <div class="grid-controls">
+      <button class="btn-switch" @click="toggleCardType">
+        <span v-if="cardType == 'list'" :title="w.grid">
+          <svg class="icon" width="16" height="16" fill="currentColor">
+            <use xlink:href="@/assets/img/bootstrap-icons.svg#grid"></use>
+          </svg>
+        </span>
+        <span v-if="cardType == 'grid'" :title="w.list">
+          <svg class="icon" width="16" height="16" fill="currentColor">
+            <use xlink:href="@/assets/img/bootstrap-icons.svg#view-list"></use>
+          </svg>
+        </span>
+      </button>
+    </div>
+  </div>
   <!-- <pre>{{ data }}</pre> -->
-  <div class="collection_cards_wrapper">
+  <div v-if="cardType == 'list'" class="collection_cards_wrapper">
     <CollectionCard v-for="collection in data.data" :key="collection.id" :entry="collection" />
+  </div>
+  <div v-if="cardType == 'grid'" class="collection_cards_wrapper card-grid">
+    <CollectionCardGrid v-for="collection in data.data" :key="collection.id" :entry="collection" />
   </div>
 </template>
 <style scoped lang="scss">
-  .collection_cards_wrapper {
-    width: min(100%, 100ch);
-    margin-inline: auto;
-  }
-  .collection_count {
-    // text-align: center;
-    width: min(100%, 100ch);
-    margin: 1rem auto;
-  }
-</style>
+.collection_cards_wrapper {
+  width: min(100%, 1200px);
+  margin-inline: auto;
+  padding: 1rem 0;
+}
+
+.card-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  // grid-template-columns: repeat(5,1fr);
+  grid-gap: 1rem;
+}
+
+.grid-control-bar {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  width: min(100%, 1200px);
+  margin: 132px auto 0;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  background-color: #fff;
+}
+
+.collections-counter {
+  font-size: 0.85rem;
+}
+
+.btn-switch {
+  margin: 0;
+  padding: 0;
+  width: auto;
+  overflow: visible;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  outline: none;
+}
+
+.sort-controls {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 1rem;
+}
+
+.filter-label,
+.sort-label,
+.order-label {
+  display: flex;
+  align-items: center;
+  margin: 0;
+  padding: .5rem;
+  line-height: 1;
+  // background-color: var(--clr-base-d550);
+  border-top-left-radius: 6px;
+  border-bottom-left-radius: 6px;
+  // border: 1px solid var(--clr-base-d400);
+  border-right: 0;
+}
+
+.sort-select,
+.order-select {
+  flex-grow: 1;
+  margin: 0;
+  padding: .5rem .25rem .5rem .25rem;
+  font-size: .85rem;
+  // color: var(--clr-base-l200);
+  line-height: 1;
+  // background-color: var(--clr-base-d550);
+  // border: 1px solid var(--clr-base-d400);
+  border-top-right-radius: 6px;
+  border-bottom-right-radius: 6px;
+}
+
+.input-group {
+  display: flex;
+  flex-grow: 1;
+}
+
+.icon {
+  width: 1.5rem;
+  height: 1.5rem;
+}
+
+// main.scrolled .gws-grid-control-bar{
+//   margin-top: 80px;
+// }</style>
