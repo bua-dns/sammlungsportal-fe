af992073e42816004b41160445de3ba737ddf370
grandgeorg <viktor@grandgeorg.de>
2023-12-07 22:44:03 +0100

    rm page

--- pages/entdecken.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="page">
-    <h1 class="text-center">{{ w.page_discover }}</h1>
-  </div>
-</template>
-<script setup>
-  const theme = useState('theme');
-  const w = theme.value.data.wording.de;
-</script>
