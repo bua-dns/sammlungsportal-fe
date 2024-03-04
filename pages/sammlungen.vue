201042454934f53a03e34a25c042ef8bcb23e755
grandgeorg <viktor@grandgeorg.de>
2024-03-04 19:41:31 +0100

    add filter state bar

--- pages/sammlungen.vue
+++ pages/sammlungen.vue
@@ -552,6 +552,7 @@ onMounted(() => {
       </div>
     </div>
     <!-- <pre>{{ terms }}</pre> -->
+    <!-- <pre>{{ termFilter }}</pre> -->
     <div v-if="showFilters" class="filter-control-bar">
       <div class="filter-control-bar-controls">
         <div class="form-check form-switch gws-form-switch-right d-flex justify-content-end align-items-center">
@@ -592,6 +593,26 @@ onMounted(() => {
       <CollectionCardDetails :collection="getCollectionById(activeCollectionId)" :termFilter="termFilter"
         :activeCollectionId="activeCollectionId" @set-term-filter="setTermFilter"
         @set-active-collection-id="setActiveCollectionId" />
+    </div>
+  </div>
+  <div class="grid-control-bar filter-state">
+    <dl class="display-element">
+      <dt>{{ w.collections_of }}:</dt>
+      <dd v-if="termFilter.current_keeper && termFilter.current_keeper.length > 0"
+        v-for="(keeper, idx) in termFilter.current_keeper" :key="'filter-state-keeper-' + idx">
+        {{ keeper }}
+      </dd>
+      <dd v-else>{{ w.all_keepers }}</dd>
+    </dl>
+    <div class="display-element-container">
+      <template v-for="(terms, taxonomy) in termFilter" :key="'filter-state-terms-' + taxonomy">
+        <dl v-if="taxonomy !== 'current_keeper' && terms.length > 0" class="display-element">
+          <dt>{{ w[taxonomy] }}:</dt>
+          <dd v-for="(term, idx) in terms" :key="'filter-state-term-' + idx">
+            {{ term }}
+          </dd>
+        </dl>
+      </template>
     </div>
   </div>
   <div class="collections_display" id="collections_display">
@@ -683,6 +704,43 @@ onMounted(() => {
   border: 1px solid #ccc;
   border-radius: 8px;
   background-color: #fff;
+
+  &.filter-state {
+    margin-top: 1rem;
+
+    .display-element-container {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 0.5rem;
+      margin: 0.5rem 0 0;
+    }
+
+    dl.display-element {
+      margin: 0;
+      line-height: 1;
+
+      dt {
+        display: inline-block;
+        font-weight: bold;
+      }
+
+      dd {
+        display: inline-block;
+        margin: 0 0 0 0.5rem;
+        font-size: 0.9rem;
+        font-weight: 300;
+        line-height: 1;
+        padding: 0.4em 0.6em;
+        border: 0;
+        border-radius: 4px;
+        background-color: var(--color-base-d0);
+        border-color: var(--color-nav-brd);
+        border-style: solid;
+        border-width: 1px;
+        color: #000;
+      }
+    }
+  }
 }
 
 .basic-controls {
