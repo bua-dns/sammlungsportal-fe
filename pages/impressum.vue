81ad1ff038b7336091d9c12647e75b18d82432d5
culturetogo <m.mueller@culture-to-go.de>
2024-03-11 07:22:16 +0100

    made page module semi-generic

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -2,19 +2,24 @@
 /* Used auto-imported composables: projectConfig */
 const theme = useState("theme")
 const w = theme.value.data.wording.de
-import { parseMarkdown } from "~/utils/parseMarkdown"
-const content = ref("")
-const { data } = await useFetch(`${projectConfig.dataBaseUrl}/pages?filter[slug][_eq]=imprint`)
-content.value = await parseMarkdown(data.value.data[0].content)
 // useHead({ title: data.value.data[0].title });
+
+// config for specific page
+
+// DEV: replace by slug from path
+const slug = 'impressum'
+const titleWording = 'page_imprint'
+
+const { data } = await useFetchPage(slug)
+const pageContent = data.value.data[0].page_content
 </script>
 
 <template>
   <Head>
-    <Title>{{ w.page_imprint }}</Title>
+    <Title>{{ w.page_contact }}</Title>
   </Head>
-  <div class="page">
-    <h1 class="text-center">{{ w.page_imprint }}</h1>
-    <ContentRendererMarkdown v-if="content" :value="content" />
+  <div class="page" v-if="data">
+    <h1 class="text-center">{{ w[titleWording] }}</h1>
+    <div class="page-content" v-html="pageContent"/>
   </div>
 </template>
