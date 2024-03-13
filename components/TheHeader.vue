4af6379e6a69bb8219cb4659f44dd742e5a05b9b
culturetogo <m.mueller@culture-to-go.de>
2024-03-13 11:24:50 +0100

    reorga and renaming of navbar css

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,14 +1,44 @@
+<script setup>
+const theme = useState("theme");
+const w = theme.value.data.wording.de;
+
+const navState = useState("nav", () => "close");
+const toggleNav = () => {
+  navState.value = navState.value === "close" ? "open" : "close";
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+
+const scrollOffset = 40;
+const scrollState = useState("scroll", () => false);
+const handleScroll = () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > scrollOffset && !navbar.classList.contains("shrink")) {
+    navbar.classList.add("shrink");
+    scrollState.value = true;
+  } else if (window.scrollY <= scrollOffset && navbar.classList.contains("shrink")) {
+    navbar.classList.remove("shrink");
+    scrollState.value = false;
+  }
+};
+</script>
+
 <template>
-  <header class="navbar fixed-top">
-    <nuxt-link to="/" class="navbar__brand" v-if="true">
-      <svg
+  <header class="navbar fixed-top dns-page-header">
+    <nuxt-link to="/" class="dns-branding" v-if="true">
+      <div class="dns-logo-container">      <svg
         id="Ebene_1"
         data-name="Ebene 1"
         xmlns="http://www.w3.org/2000/svg"
         width="119"
         height="89.25"
         viewBox="0 0 119 89.25"
-        class="navbar__logo"
+        class="dns-navbar-logo"
       >
         <line
           x1="22.31"
@@ -43,13 +73,14 @@
         <circle cx="93.22" cy="18.84" r="18.84" style="fill: #9acd00; stroke-width: 0px" />
         <circle cx="18.84" cy="70.41" r="18.84" style="fill: #c30e20; stroke-width: 0px" />
         <circle cx="100.16" cy="63.47" r="18.84" style="fill: #00376b; stroke-width: 0px" />
-      </svg>
-      <div class="navbar__title_container">
-        <div class="navbar__title title">{{ w.site_title }}</div>
-        <div class="navbar__subtitle title">{{ w.site_subtitle }}</div>
+      </svg></div>
+
+      <div class="dns-site-title-container">
+        <div class="dns-site-title title">{{ w.site_title }}</div>
+        <div class="dns-site-subtitle title">{{ w.site_subtitle }}</div>
       </div>
     </nuxt-link>
-    <nav class="navbar__links">
+    <nav class="dns-main-navigation">
       <ul>
         <li>
           <nuxt-link to="/">{{ w.page_start }}</nuxt-link>
@@ -62,7 +93,7 @@
         </li>
       </ul>
     </nav>
-    <div class="navbar__controls">
+    <div class="dns-navbar-controls">
       <button
         @click="toggleNav"
         :class="navState === 'open' ? 'burger open' : 'burger'"
@@ -76,32 +107,143 @@
     </div>
   </header>
 </template>
-<script setup>
-const theme = useState("theme");
-const w = theme.value.data.wording.de;
 
-const navState = useState("nav", () => "close");
-const toggleNav = () => {
-  navState.value = navState.value === "close" ? "open" : "close";
-};
-
-onMounted(() => {
-  window.addEventListener("scroll", handleScroll);
-});
-onUnmounted(() => {
-  window.removeEventListener("scroll", handleScroll);
-});
-
-const scrollOffset = 40;
-const scrollState = useState("scroll", () => false);
-const handleScroll = () => {
-  const navbar = document.querySelector(".navbar");
-  if (window.scrollY > scrollOffset && !navbar.classList.contains("shrink")) {
-    navbar.classList.add("shrink");
-    scrollState.value = true;
-  } else if (window.scrollY <= scrollOffset && navbar.classList.contains("shrink")) {
-    navbar.classList.remove("shrink");
-    scrollState.value = false;
+<style scoped lang="scss">
+.dns-page-header {
+  .dns-logo-container {
+    margin: 0 1.5rem 0 .5rem;
   }
-};
-</script>
+}
+// DEV integrate with above
+
+.navbar {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 200;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--color-bg);
+  line-height: 1;
+  // border-bottom: 1px solid var(--color-border);
+  // box-shadow: 0 0 8px 0 rgba(0,0,0,0.8);
+  box-shadow: 0 0 0.625rem 0 rgba(0, 0, 0, 0.4);
+  .dns-branding {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    text-decoration: none;
+    color: var(--color-text);
+    .dns-navbar-logo {
+      width: 76px;
+      height: 76px;
+      flex-grow: 1;
+      flex-shrink: 0;
+      box-sizing: content-box;
+      // padding: 0.5rem 0 0.5rem 0.5rem;
+      padding: 0.75rem 0 0.75rem 0.5rem;
+      transition: height 0.25s, padding 0.25s;
+    }
+    .dns-site-title-container {
+      margin-left: 0.5rem;
+    }
+    .dns-site-title {
+      font-weight: 900;
+      font-size: 2rem;
+      // font-size: clamp(1rem, 3.3333vw, 2rem);
+      font-size: clamp(1.25rem, 4.1667vw, 2rem);
+      line-height: 1.125;
+      transition: all 0.25s;
+    }
+    .dns-site-subtitle {
+      font-weight: 400;
+      font-size: 1.27rem;
+      // font-size: clamp(1rem, 3.3333vw, 1.27rem);
+      font-size: clamp(0.8rem, 2.6667vw, 1.27rem);
+      line-height: 1.125;
+      transition: all 0.25s;
+    }
+  }
+  .dns-navbar-controls {
+    display: flex;
+    flex-direction: row;
+    align-items: stretch;
+  }
+  .dns-main-navigation {
+    display: none;
+    @media (min-width: 768px) {
+      display: flex;
+      align-items: center;
+      ul {
+        display: flex;
+        flex-direction: row;
+        align-items: stretch;
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        li {
+          margin: 0;
+          padding: 0;
+          a {
+            display: block;
+            // padding: 0.5rem 1rem;
+            // padding: 11px;
+            padding: 1rem;
+            text-decoration: none;
+            color: var(--color-text);
+            transition: all 0.25s;
+            &:hover,
+            &:focus,
+            &:active,
+            &.router-link-exact-active:hover,
+            &.router-link-exact-active:focus,
+            &.router-link-exact-active:active {
+              color: var(--color-text-inv);
+              // background-color: var(--color-link-bg);
+              background-color: var(--color-nav-hover-bg);
+            }
+            &.router-link-exact-active,
+            &.router-link-exact-active,
+            &.router-link-exact-active {
+              color: var(--color-text-inv);
+              background-color: var(--color-nav-active-bg);
+            }
+          }
+        }
+      }
+    }
+  }
+  &.shrink {
+    .dns-branding {
+      .dns-navbar-logo {
+        // padding: 0.125rem 0 0.125rem 0.25rem;
+        padding: 0.25rem 0 0.25rem 0.25rem;
+        width: auto;
+        // height: 2rem;
+        height: 2.5rem;
+      }
+      .dns-site-title {
+        font-size: 1.25rem;
+        margin-left: 0.25rem;
+      }
+      .dns-site-subtitle {
+        font-size: 0.8rem;
+        margin-left: 0.25rem;
+      }
+    }
+  }
+  button.burger:not([disabled]):not([tabindex^="-"]):focus {
+    outline: 0;
+    background-color: var(--color-nav-hover-bg);
+    color: #fff;
+    .burger__line {
+      background-color: #fff;
+    }
+  }
+}
+</style>
+
