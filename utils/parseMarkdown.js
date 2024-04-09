0193e5aeeb69e941e8f972cd96409d1b850f698d
grandgeorg <viktor@grandgeorg.de>
2024-04-09 21:32:00 +0200

    rm parsemarkdown

--- utils/parseMarkdown.js
+++ utils/parseMarkdown.js
@@ -1,2 +1,2 @@
-import markdownParser from '@nuxt/content/transformers/markdown';
-export const parseMarkdown = md => markdownParser.parse('custom.md', md);
+// import markdownParser from '@nuxt/content/transformers/markdown';
+// export const parseMarkdown = (md) => markdownParser.parse("custom.md", md);
