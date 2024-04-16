ae195c13db6cd29998fc1665c301e6d594d87568
culturetogo <m.mueller@culture-to-go.de>
2024-04-16 17:28:54 +0200

    projects page with projects listing, CardsPage

--- pages/projekte.vue
+++ pages/projekte.vue
@@ -13,6 +13,10 @@ const titleWording = 'page_projekte'
 const { data } = await useFetchPage(slug)
 const page = data.value.data[0]
 
+const projectsData = useState('projects');
+const projects = projectsData.value.data
+  .filter(project => project.show_on_projects_page === 'true');
+
 </script>
 
 <template>
@@ -21,7 +25,7 @@ const page = data.value.data[0]
     <Title>{{ w.page_projekte }}</Title>
   </Head>
   <div class="page p_dns-page" v-if="data && page.status === 'published'">
-    <pre v-if="false">{{ page }}</pre>
+    <pre v-if="false">{{ projects }}</pre>
     <h1 class="mb-4 text-center">{{ w[titleWording] }}</h1>
     <template v-if="!page.display_sidebar">
       <div class="page-content" v-html="page.page_content" />
@@ -35,13 +39,18 @@ const page = data.value.data[0]
             <img :src="projectConfig.imageBaseUrl + '/' + page.sidebar_header_image + '?key=sidebar-header'"
               alt="sidebar image" />
           </div>
-          <div class="sidebar-content"
-            v-if="page.sidebar_content"
-            v-html="page.sidebar_content"
-          />
+          <div class="sidebar-content" v-if="page.sidebar_content" v-html="page.sidebar_content" />
         </div>
       </div>
     </template>
+    <div v-if="true" class="projects-listing page-card-grid">
+      <!-- <pre>{{ projects.data[0] }}</pre> -->
+      <div class="project-display" 
+        v-for="(project, idx) in projects" :key="`page-card-${idx}`"
+      >
+        <CardPageProject :cardContent="project" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -61,6 +70,11 @@ const page = data.value.data[0]
         }
       }
     }
+  }
+  .page-card-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(var(--feature-card-width), 1fr));
+    gap: 1.5rem;
   }
 }
 @media screen and (min-width: 768px){
