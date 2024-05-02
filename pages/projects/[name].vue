03f0bd3ae32533d3dd05bc0ad0d757e8f49b5509
culturetogo <m.mueller@culture-to-go.de>
2024-05-02 11:54:05 +0200

    styling project detail page

--- pages/projects/[name].vue
+++ pages/projects/[name].vue
@@ -24,7 +24,6 @@ const images = project.images.map((image) => {
     <div class="projects-content-container">
       <div class="main-content">
         <div class="description" v-html="project.description"></div>
-        <ImageViewer :images="images" previewMode="gallery" previewImageWidth="240"></ImageViewer>
       </div>
       <div class="sidebar" v-if="project.display_sidebar">
         <div class="partners" v-if="project.cooperation_partners?.length">
@@ -33,23 +32,27 @@ const images = project.images.map((image) => {
             <li v-for="partner in project.cooperation_partners">
               <a :href="partner.institutions_id.website" target="_blank">
                 <img v-if="partner.institutions_id.logo"
-                  :src="`${projectConfig.imageBaseUrl}/${partner.institutions_id.logo}?key=sidebar-logo`"
-                  :alt="`Logo ${partner.institutions_id.name_short}`" />
+                :src="`${projectConfig.imageBaseUrl}/${partner.institutions_id.logo}?key=sidebar-logo`"
+                :alt="`Logo ${partner.institutions_id.name_short}`" />
               </a>
             </li>
           </ul>
         </div>
-
         <div class="sidebar-content" v-html="project.sidebar_content" v-if="project.sidebar_content"></div>
       </div>
+    </div>
+    <div class="images mt-4">
+      <ImageViewer :images="images" previewMode="gallery" previewImageWidth="240"></ImageViewer>
     </div>
   </div>
 </template>
 <style lang="scss">
 .page.projects {
   .projects-content-container {
-
     display: block;
+    h4 {
+      margin: 0 0 1rem;
+    }
   }
   .sidebar {
     display: none;
