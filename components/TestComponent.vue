94ba7674aa6b886b0cbd68e5b08d0128a317aa91
culturetogo <m.mueller@culture-to-go.de>
2024-06-04 07:53:15 +0200

    first steps state with objects

---
+++ components/TestComponent.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+  store: { 
+    type: String
+  }
+});
+const filterStore = useState(props.store);
+const currentList = ref('list1')
+
+function setCurrentList(list) {
+  currentList.value = list
+} 
+
+const increment = () => {
+  filterStore.value[currentList.value].push(`term${filterStore.value[currentList.value].length + 1}`)
+}
+
+
+</script>
+
+<template>
+  <button @click="increment">Increment</button>
+  <button @click="setCurrentList('list1')">set list to 1</button>
+  <button @click="setCurrentList('list2')">set list to 2</button>
+
+</template>
---
+++ pages/workbench.vue
@@ -0,0 +1,88 @@
+<script setup>
+
+const counter = useState('counter', () => {
+  return {
+    list1: ['term1'],
+    list2: ['term1']
+  }
+})
+
+</script>
+
+<template>
+
+  <div class="page image-listing-page" v-if="true">
+    <section class="cms-page">
+      <h1 class="text-center">workbench</h1>
+      <div>
+        List 1: {{ counter.list1 }}
+      </div>
+      <div>
+        List 2: {{ counter.list2 }}
+      </div>
+      <TestComponent store="counter"/>
+
+      <div class="dev-outs" v-if="false">
+        counter: <br>
+        {{ counter }}
+      </div>
+      <!-- <DEVDisplayUseStore /> -->
+    </section>
+  </div>
+</template>
+
+<style lang='scss'>
+button {
+  display: block;
+  margin: 1rem
+}
+.image-listing-page {
+  .listing {
+    .entry {
+      scroll-margin-top: 88px;
+      display: flex;
+      flex-direction: column;
+      gap: 1.5rem;
+
+      @media screen and (min-width: 768px) {
+        flex-direction: row;
+        gap: 2.5rem;
+      }
+
+      .image-part {
+        flex: 1;
+
+        img {
+          width: 100%;
+          height: auto;
+        }
+
+        .credits {
+          font-size: var(--font-size-text-extra-small);
+        }
+      }
+
+      .text-part {
+        flex: 1;
+        font-size: var(--font-size-text-small);
+
+        .description {
+          font-size: var(--font-size-text-small);
+
+          p {
+            margin: 0;
+          }
+        }
+
+        .link {
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+          gap: 0.5rem;
+        }
+      }
+    }
+  }
+}
+</style>
+
