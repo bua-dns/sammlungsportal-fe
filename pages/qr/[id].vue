3c4acb8624a77d90ceb358fabad0325ffd519130
culturetogo <m.mueller@culture-to-go.de>
2024-06-11 14:44:52 +0200

    restored events page

---
+++ pages/qr/[id].vue
@@ -0,0 +1,11 @@
+<script setup>
+const route = useRoute();
+const id = ref(route.params.id);
+await navigateTo(`/?utm_source=qr-${id.value}`)
+</script>
+
+<template>
+  <div>
+    <h1>QR</h1>
+  </div>
+</template>
