d1a871a570a0e1748adef93f35581cc81400822a
culturetogo <m.mueller@culture-to-go.de>
2024-07-10 11:13:18 +0200

    formatting ikb item display component

--- components/IKBItem.vue
+++ components/IKBItem.vue
@@ -10,16 +10,15 @@ const props = defineProps({
 
 <template>
   <div class="ikb-item">
-    <!-- <img
-      :src="`https://rs.cms.hu-berlin.de/ikb_mediathek/pages/download.php?ref=${item.mr_ref}&size=pre&ext=jpg&page=1&alternative=-1&k=&noattach=true`"
-      alt="" loading="lazy"> -->
     <LightBoxIkbItem :objectId="item.mr_ref" />
     <div class="basic-info">
-
-
-      <a :href="`https://rs.cms.hu-berlin.de/ikb_mediathek/pages/view.php?ref=${item.mr_ref}`" target="_blank">Lehrbild
+      <div class="ikb-link">
+        <a :href="`https://rs.cms.hu-berlin.de/ikb_mediathek/pages/view.php?ref=${item.mr_ref}`" target="_blank">Lehrbild
         in der IKB-Datenbank</a>
-      <br>{{ item.dns_title }}
+      </div>
+      <div class="ikb-info">
+        {{ truncateText(convertLineBreaks(item.dns_title), 80) }}
+      </div>
     </div>
   </div>
 </template>
@@ -28,11 +27,20 @@ const props = defineProps({
   .ikb-item {
     width: 220px;
     margin: 10px;
-    img {
-      width: 100%;
-    }
     .basic-info {
       font-size: 0.8rem;
+      .ikb-link {
+        margin-bottom: .25rem;
+        a {
+          text-decoration: none;
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+      }
+      .ikb-info {
+        
+      }
     } 
   }
 </style>
